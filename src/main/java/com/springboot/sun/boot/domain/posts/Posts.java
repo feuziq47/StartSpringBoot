99b5f10bee163aaa79c23efd5664e110c1b624b9
Sun <feuziq47@gmail.com>
2020-08-31 23:33:01 +0900

    post repo 추가 및 api 컨트롤러 추가

---
+++ src/main/java/com/springboot/sun/boot/domain/posts/Posts.java
@@ -0,0 +1,31 @@
+package com.springboot.sun.boot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity     // 테이블과 링크될 클래스임을 나타냄. 클래스 = 카멜케이스 <-> 테이블 = 언더스코어
+public class Posts {
+    @Id     // 해당 테이블의 PK 필드를 나타냄
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/springboot/sun/boot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.springboot.sun.boot.service.posts;
+
+import com.springboot.sun.boot.domain.posts.PostsRepository;
+import com.springboot.sun.boot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
