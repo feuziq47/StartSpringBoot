4a89e7208b30bec34633df821207e8b42d42aef2
Sun <feuziq47@gmail.com>
2020-10-10 22:20:26 +0900

    전체 조회 화면 만들기

--- src/main/java/com/springboot/sun/boot/service/posts/PostsService.java
+++ src/main/java/com/springboot/sun/boot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.springboot.sun.boot.service.posts;
 
 import com.springboot.sun.boot.domain.posts.Posts;
 import com.springboot.sun.boot.domain.posts.PostsRepository;
+import com.springboot.sun.boot.web.dto.PostsListResponseDto;
 import com.springboot.sun.boot.web.dto.PostsResponseDto;
 import com.springboot.sun.boot.web.dto.PostsSaveRequestDto;
 import com.springboot.sun.boot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +34,10 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id= "+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
